f5fd01388fc313b06dc5eaedf0d379ff7b1e4aa4
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-10-11 21:55:16 +0600

    chart-added

--- src/Components/Statics/Statics.jsx
+++ src/Components/Statics/Statics.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { HomeDataContext } from '../../Layout/Main';
 
 const Statics = () => {
+    const datas = useContext(HomeDataContext)
+    const data = datas.data
     return (
         <div>
-            <h1>Statics</h1>
+            <div className='p-7'>
+                <h1 className='text-5xl'>This sites is about to the all total questiones details show to this chart  </h1>
+            </div>
+            <div className='flex  justify-center'>
+                <LineChart width={400} height={400} data={data}>
+                    <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
         </div>
     );
 };
